544521ace807b7722c2dea52b2c705bbdfcb9c20
Dylan Chen <hchennyc@outlook.com>
2017-09-03 22:07:44 -0400

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+var app= require ('express')();
+var axios = require('axios');
+var bodyParser = require('body-parser');
+
+const BASE_URL = "https://andruxnet-random-famous-quotes.p.mashape.com";
+const PORT = process.env.PORT || 8081;
+
+app.use(require('cors')());
+app.use( bodyParser.json()); 
+
+var MakeRequest = (cat, count,res)=>{
+    axios.get(`${BASE_URL}/?cat=${cat}&count=${count}`,
+        {headers:{"X-Mashape-Key":"a9YGCRpdKtmshWMqCZlcdkHcuGEOp1n0Uk9jsnmJM7DfPwwQBG"}})
+        .then(response=>{
+            res.json(response.data);
+        });
+};
+
+app.route(['/GetQuotes'])
+    .get((req,res)=>{
+        MakeRequest(req.query.cat,req.query.count,res);
+    })
+    .post((req,res)=>{
+        MakeRequest(req.body['cat'],req.body['count'] ,res);
+    });
+
+app.listen(PORT,()=>{
+    console.log("Listening to ",PORT);
+});
